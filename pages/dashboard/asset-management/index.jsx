30755ab3542cdce7aa40e789da4143aef287717d
abdul hannan <abdul.hannan@plaxonic.com>
2023-09-19 17:09:59 +0530

    dialog

--- pages/dashboard/asset-management/index.jsx
+++ pages/dashboard/asset-management/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import MainLayout from 'proj-components/MainLayout'
+import Button from '@/components/atoms/button'
+import { Text1 } from '@/components/atoms/field'
 const AssetMangement = () => {
   return (
    <>
     <MainLayout>
     <div>
-      Asset Management
+       <div className='flex justify-between items-center'>
+        <Text1 size='2xl'>All Assets</Text1>
+         <Button>ADD ASSET</Button>
+       </div>
     </div>
     </MainLayout>
    </>
